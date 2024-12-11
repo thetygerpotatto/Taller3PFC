b5a2f5cfa98f5739428f825f8e8996c4136398fb
Diego Fernando Lenis Delgado <161733208+Diego-F-Lenis-c137@users.noreply.github.com>
2024-12-11 16:19:11 -0500

    Add files via upload
    Adición algoritmo multiplicación matrices strassen

--- app/src/main/scala/taller/Matrices.scala
+++ app/src/main/scala/taller/Matrices.scala
@@ -116,6 +116,52 @@ class Matrices {
       }                    // Igualmente multMatrizRecPar es el doble de rapida que multMatrizRec cuando usamos multMatrizRec
     }                      // en lugar de multMatriz
   }
+
+
+  def strassensAlg(m1: Matriz, m2: Matriz):Matriz = {
+
+    assert(m1.length == m2.length)
+
+    val p1 = (m1(0)(0)) * (m2(0)(1) - m2(1)(1))
+    val p2 = (m1(0)(0) + m1(0)(1)) * (m2(1)(1))
+    val p3 = (m1(1)(0) + m1(1)(1)) * (m2(0)(0))
+    val p4 = (m1(1)(1)) * (m2(1)(0) - m2(0)(0))
+    val p5 = (m1(0)(0) + m1(1)(1)) * (m2(0)(0) + m2(1)(1))
+    val p6 = (m1(0)(1) - m1(1)(1)) * (m2(1)(0) + m2(1)(1))
+    val p7 = (m1(0)(0) - m1(1)(0)) * (m2(0)(0) + m2(0)(1))
+
+    val c11 = p5 + p4 - p2 + p6
+    val c12 = p1 + p2
+    val c21 = p3 + p4
+    val c22 = p5 + p1 - p3 - p7
+    val result: Vector[Vector[Int]] = Vector(
+      Vector(c11, c12),
+      Vector(c21, c22))
+    
+    result
+  }
+
+  def strassensMatrixMult(m1: Matriz, m2: Matriz) : Matriz = {
+    assert(isPowerOfTwo(m1.length) && isPowerOfTwo(m2.length) && m1.length == m2.length) 
+    val length = m1.length
+    if (length <= 2) strassensAlg(m1, m2)
+    else {
+      val subMl = length/2
+      val subMats = Vector.tabulate(2, 2)((i , j) => 
+        strassensMatrixMult(
+          strassensMatrixMult(subMatriz(m1, i*subMl, 0, subMl), subMatriz(m2, 0, j*subMl, subMl)),
+          strassensMatrixMult(subMatriz(m1, i*subMl, subMl, subMl), subMatriz(m2, subMl, j*subMl, subMl))
+          )
+          )
+          
+      val result: Vector[Vector[Int]] = Vector(
+        subMats(0)(0)(0) ++ subMats(0)(1)(0),
+        subMats(0)(0)(1) ++ subMats(0)(1)(1),
+        subMats(1)(0)(0) ++ subMats(1)(1)(0),
+        subMats(1)(0)(1) ++ subMats(1)(1)(1))
+      result
+    }
+  }
   //Función para restar dos matrices de igual tamaño
   def restaMatriz(m1: Matriz , m2: Matriz ) : Matriz ={
     assert(m1.length == m2.length)
